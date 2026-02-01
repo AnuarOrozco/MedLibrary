5201cd64ae9ff3293c7e99f6147359456bc5d4c2
AnuarOrozco <anuarorozzco@gmail.com>
2026-02-01 13:19:35 -0600

    chore: initial project setup

---
+++ src/main/java/com/medlibrary/MedlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.medlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medlibrary/MedlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
